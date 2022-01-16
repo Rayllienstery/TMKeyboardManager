228e2bdb7e1b27ebfff557b737c9d8988f0b76b3
Konstantin Kolosov <rayleefx@protonmail.com>
2022-01-17 00:23:30 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TMKeyboardManager
+
+A description of this package.
---
+++ Sources/TMKeyboardManager/TMKeyboardManager.swift
@@ -0,0 +1,6 @@
+public struct TMKeyboardManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
