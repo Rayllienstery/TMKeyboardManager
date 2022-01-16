68dcddb713dbee90925d96b0485f398f25e863f2
Konstantin Kolosov <rayleefx@protonmail.com>
2022-01-17 00:33:35 +0200

    Uploaded basic logic

--- Sources/TMKeyboardManager/TMKeyboardManager.swift
+++ Sources/TMKeyboardManager/TMKeyboardManager.swift
@@ -1,6 +1,42 @@
-public struct TMKeyboardManager {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+@objc protocol TMKeyboardProtocol {
+    @objc optional func didShowKeyboard()
+    @objc optional func didHideKeyboard()
+}
+
+public extension UIViewController {
+
+    func initKeyboardObserver() {
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func deinitKeyboardObserver() {
+        NotificationCenter.default.removeObserver(
+            self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(
+            self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size {
+            var frame = self.view.frame
+            frame.origin.y = -keyboardSize.height
+            UIView.animate(withDuration: 0.12) {
+                self.view.frame = frame
+            }
+        }
+        (self as? TMKeyboardProtocol)?.didShowKeyboard?()
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        var frame = self.view.frame
+        frame.origin.y = 0
+        UIView.animate(withDuration: 0.12) {
+            self.view.frame = frame
+        }
+        (self as? TMKeyboardProtocol)?.didHideKeyboard?()
     }
 }
