dd6aa3d1658c93b29768b63873fddc385fd77f54
Konstantin Kolosov <rayleefx@protonmail.com>
2022-01-17 01:03:58 +0200

    Added a little description for the main UIViewController extension

--- Sources/TMKeyboardManager/TMKeyboardManager.swift
+++ Sources/TMKeyboardManager/TMKeyboardManager.swift
@@ -9,7 +9,10 @@ import UIKit
     @objc optional func willHideKeyboard()
 }
 
-/// If you want
+/// The main purpose of the library is to automate and minimize interaction with the elements that call the keyboard.
+/// initKeyboard() adds two observers to the NotificationCenter that monitor the state of the keyboard and then, if necessary, change the position of view.frame so that the input field is always visible
+///
+/// deinitKeyboard(): Do not forget that NotificationCenter puts a load on RAM, does not allow you to release resources, and therefore do not forget to release unnecessary observers, to make this, call deinitKeyboard() on viewDidDissapear()
 public extension UIViewController {
     // MARK: - Public
     func initKeyboard() {
