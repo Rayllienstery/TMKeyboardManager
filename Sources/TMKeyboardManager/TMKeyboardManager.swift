0bb602dca837f9e3ad5b52d57bc5a26439efe42d
Konstantin Kolosov <rayleefx@protonmail.com>
2022-01-17 04:46:23 +0200

    Updated documentation

--- Sources/TMKeyboardManager/TMKeyboardManager.swift
+++ Sources/TMKeyboardManager/TMKeyboardManager.swift
@@ -11,6 +11,7 @@ import UIKit
 
 /// The main purpose of the library is to automate and minimize interaction with the elements that call the keyboard.
 /// initKeyboard() adds two observers to the NotificationCenter that monitor the state of the keyboard and then, if necessary, change the position of view.frame so that the input field is always visible
+/// Use it on the ViewDidAppear()
 ///
 /// deinitKeyboard(): Do not forget that NotificationCenter puts a load on RAM, does not allow you to release resources, and therefore do not forget to release unnecessary observers, to make this, call deinitKeyboard() on viewDidDissapear()
 public extension UIViewController {
